7dd295ba79b80bd7f78ddaf59c766df895f0ab9b
unknown <amaka013@gmail.com>
2021-03-15 08:51:05 +0100

    Updated files

--- Docker_Django/settings.py
+++ Docker_Django/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT=os.path.join(BASE_DIR,'Static')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
