fc479cb7af3960847319a860d17c9dbf9c8704c2
unknown <amaka013@gmail.com>
2021-03-20 23:14:17 +0100

    Changes

--- Docker_Django/settings.py
+++ Docker_Django/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Helloworld'
+    'Helloworld',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
